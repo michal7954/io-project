e3d86a6177347bcfbbb4b8bb07d541f3db147124
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2021-11-28 14:42:25 +0100

    Dodanie globalnej pamięci

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
+import storage
 from classes.Store import Store
 from classes.Room import Room
+from classes.Reservation import Reservation
 
 
 def main():
-    
-    storage = {
-        "rooms": Store(Room),
-        "reservations": Store(Room),
-        "services": Store(Room),
-        "guests": Store(Room),
-    }
+    global storage
+
+    storage.rooms = Store(Room)
+    storage.reservations = Store(Reservation)
+    # storage.services = Store(Services)
 
     def listener():
-        nonlocal storage
 
         while True:
             line = input()
@@ -21,10 +20,17 @@ def main():
             if len(params)==0:
                 continue
             
-            store = storage.get(params[0])
+            store = params[0]
+            if store == 'rooms':
+                store = storage.rooms
+            elif store == 'reservations':
+                store = storage.reservations
+            # elif store == 'services':
+            #     store = storage.services
 
             store.call(params)
             
+            # todo zaawansowana weryfikacja i obsługa błędów
             # try:
             #     store.call(params)
             # except:
