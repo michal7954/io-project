29bcf1d7a94d0c7e93eb59fc917be54d029cceb5
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2021-11-28 14:43:52 +0100

    Warunkowe dodawanie elementów, refaktoryzacja

--- src/classes/Store.py
+++ src/classes/Store.py
@@ -1,54 +1,57 @@
 class Store():
     def __init__(self, Template):
         self.elements = {}
-        self.nextKey=0
+        self.nextKey = 1
         self.Template = Template
 
     def generateKey(self):
         key = self.nextKey
-        self.nextKey+=1
+        self.nextKey += 1
         return key
 
-    def prepareObject(self, params):
-        return self.Template(params)
+    def prepareObject(self, key, params):
+        element = self.Template(key, params)
+        if element.objectStatus == 'ok':
+            return element
+        print('Object creation error')
+        return None
 
     def call(self, params):
         operator = params[1]
-        if operator=='add':
-            element = self.prepareObject(params[2:])
-            self.add(element)
 
-        elif operator=='rem':
-            key = int(params[2])
-            self.rem(key)
-
-        elif operator=='mod':
-            key = int(params[2])
-            element = self.prepareObject(params[3:])
-            self.mod(key, element)
-
-        elif operator=='get':
+        if operator == 'add':
+            self.add(params)
+        elif operator == 'rem':
+            self.remove(params)
+        elif operator == 'mod':
+            self.modify(params)
+        elif operator == 'get':
             key = int(params[2])
             self.get(key)
-
-        elif operator=='list':
+        elif operator == 'list':
             self.listElements()
 
-    def add(self, element):
+    def add(self, params):
         key = self.generateKey()
-        self.elements[key]=element
-        return key
+        element = self.prepareObject(key, params[2:])
+        if element:
+            self.elements[key] = element
+            return key
 
-    def rem(self, key):
+    def modify(self, params):
+        key = int(params[2])
+        element = self.prepareObject(key, params[3:])
+        if element:
+            self.elements[key] = element
+            return key
+
+    def remove(self, params):
+        key = int(params[2])
         self.elements.pop(key)
 
-    def mod(self, key, element):
-        self.elements[key]=element
-
     def get(self, key):
-        print(self.elements.get(key))
+        return self.elements.get(key)
 
     def listElements(self):
-        for key, element in self.elements.items():
-            string = '#' + str(key) + ' ' + str(element)
-            print(string)
+        for element in self.elements.values():
+            print(str(element))
